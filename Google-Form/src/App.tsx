f6cde72ea6c1f1f0764040e90791c8d462c2394b
GonzaloTuset <110003160+GonzaloTuset@users.noreply.github.com>
2023-09-09 18:26:00 -0300

    funciona

--- Google-Form/src/App.tsx
+++ Google-Form/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
     <main className='text-white'>
       <div className='flex flex-col pl-[100px] pt-[100px]'>
         <li className='pb-[10px] text-green-400'>Usted esta autenticado, tiene permisos para borrar su cuenta</li>
-        <li className='pb-[10px]'>Para borrar su cuenta debe poner el mail de su cuenta en el siguiente formulario</li>
+        <li className='pb-[10px]'>Para borrar su cuenta debe poner el mail de la misma en el siguiente formulario</li>
         <li className='pb-[10px]'>Al cabo de unas horas luego de enviar el formulario se eliminara su cuenta</li>
       </div>
     <EmailForm/>
