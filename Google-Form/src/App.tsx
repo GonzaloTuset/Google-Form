d6e9f53ab7858e2c2731c4980faf0b5127f890d7
GonzaloTuset <110003160+GonzaloTuset@users.noreply.github.com>
2023-09-09 12:54:01 -0300

    Form

---
+++ Google-Form/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <header>
+    <h1 className="text-3xl font-bold underline">
+      Formulario Para borrar cuenta
+    </h1>
+    </header>
+    <main>
+
+    </main>
+    </>
+  )
+}
+
+export default App
